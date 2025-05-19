f2efba59a268db80499431e9e166b8408174d250
gunmango <iamdoorpark@gmail.com>
2025-05-19 13:16:57 +0900

    메인씬 프리펩분리

---
+++ DrunkenBoard/Assets/Scripts/MainGameScene/MainGameSceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MainGameSceneManager : ASceneManager<MainGameSceneManager>
+{
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+}
