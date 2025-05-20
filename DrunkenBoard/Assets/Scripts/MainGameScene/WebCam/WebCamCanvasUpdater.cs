2b965681368e8878b51fd592849aafa6fb53c74b
gunmango <iamdoorpark@gmail.com>
2025-05-20 20:47:46 +0900

    메인게임씬 왭캠, 프리팹정리

--- DrunkenBoard/Assets/Scripts/MainGameScene/WebCam/WebCamCanvasUpdater.cs
+++ DrunkenBoard/Assets/Scripts/MainGameScene/WebCam/WebCamCanvasUpdater.cs
@@ -3,9 +3,11 @@ using UnityEngine.UI;
 
 public class WebCamCanvasUpdater : MonoBehaviour
 {
+    [SerializeField] private WebCamUpdater selfUpdater;
     [SerializeField] private WebCamUpdater originalUpdater;
     [SerializeField] private Transform contentTransform;
 
+    public WebCamUpdater SelfUpdater => selfUpdater;
     public WebCamUpdater CreateUpdater()
     {
         WebCamUpdater updater = Instantiate(originalUpdater, contentTransform);
