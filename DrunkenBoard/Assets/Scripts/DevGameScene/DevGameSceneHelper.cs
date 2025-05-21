e24439fffe2374522d13db04c971a14786f7f73c
gunmango <iamdoorpark@gmail.com>
2025-05-21 11:14:37 +0900

    왭캠 구조 변경

--- DrunkenBoard/Assets/Scripts/DevGameScene/DevGameSceneHelper.cs
+++ DrunkenBoard/Assets/Scripts/DevGameScene/DevGameSceneHelper.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class DevGameSceneHelper : MonoBehaviour
 {
-    [SerializeField] private int playerCount = 8;
+    //[SerializeField] private int playerCount = 8;
 
-    private void Start()
+    private void Awake()
+    {
+        MainGameSceneManager.Instance.ActInitialize += Initialize;
+    }
+
+    private void Initialize()
     {
         //임의로 플레이어 만들기
         
