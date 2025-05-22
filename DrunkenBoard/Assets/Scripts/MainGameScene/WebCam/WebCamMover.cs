35d38b79a69f723c710ebf9ac45b65f6ea0b7c5d
gunmango <iamdoorpark@gmail.com>
2025-05-22 09:11:19 +0900

    오타수정, 주석처리

--- DrunkenBoard/Assets/Scripts/MainGameScene/WebCam/WebCamMover.cs
+++ DrunkenBoard/Assets/Scripts/MainGameScene/WebCam/WebCamMover.cs
@@ -1,19 +1,20 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 using static DG.Tweening.DOTween;
 
 public class WebCamMover : MonoBehaviour
 {
-    [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private RectTransform rectTransform;
     [SerializeField] private float moveDuration = 2f;
 
     public void SetTo(Vector2 anchoredPosition)
     {
-        _rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.anchoredPosition = anchoredPosition;
     }
 
-    public void MoveToween(Vector2 anchoredPosition)
+    public void MoveTween(Vector2 anchoredPosition)
     {
-        _rectTransform.DOAnchorPos(anchoredPosition,moveDuration).SetEase(Ease.InOutQuad);
+        rectTransform.DOAnchorPos(anchoredPosition,moveDuration).SetEase(Ease.InOutQuad);
     }
 }
