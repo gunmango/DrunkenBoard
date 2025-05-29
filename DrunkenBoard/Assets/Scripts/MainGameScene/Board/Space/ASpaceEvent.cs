052a67b2867c783dd04a3bdeaff90e70e8911e6a
gunmango <iamdoorpark@gmail.com>
2025-05-29 10:54:25 +0900

    텍스트파일

--- DrunkenBoard/Assets/Scripts/MainGameScene/Board/Space/ASpaceEvent.cs
+++ DrunkenBoard/Assets/Scripts/MainGameScene/Board/Space/ASpaceEvent.cs
@@ -4,6 +4,7 @@ using UnityEngine.Serialization;
 public abstract class ASpaceEvent : MonoBehaviour
 {
     public ESpaceEventType SpaceEventType = ESpaceEventType.None;
+    public string EventName = string.Empty;
 
     public abstract void PlayEvent();
 }
--- DrunkenBoard/Assets/Scripts/MainGameScene/Board/Space/Space.cs
+++ DrunkenBoard/Assets/Scripts/MainGameScene/Board/Space/Space.cs
@@ -1,21 +1,23 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Space : MonoBehaviour
 {
     [SerializeField] private SpriteRendererTweener spriteTweener;
-    [SerializeField] private List<Transform> PieceSpots;
+    [SerializeField] private List<Transform> pieceSpots;
+    [SerializeField] private TextMeshPro eventName;
     public SpriteRendererTweener SpriteTweener => spriteTweener;
     
     public ESpaceEventType SpaceEventType { get; set; } = ESpaceEventType.None;
     
     public Vector3 GetPieceSpotPos(int index)
     {
-        return PieceSpots[index].position;
+        return pieceSpots[index].position;
     }
 
     public int GetSpotCount()
     {
-        return PieceSpots.Count;
+        return pieceSpots.Count;
     }
 }
