5c8e9776ecd1600d19a1fad7fc8f44dfe82538a7
gunmango <iamdoorpark@gmail.com>
2025-05-29 16:35:57 +0900

    캐릭터 애니메이션, 글씨 페이드인아웃

--- DrunkenBoard/Assets/Scripts/MainGameScene/Board/PlayerPiece/PlayerPieceAnimator.cs
+++ DrunkenBoard/Assets/Scripts/MainGameScene/Board/PlayerPiece/PlayerPieceAnimator.cs
@@ -1,14 +1,36 @@
+using Fusion;
 using UnityEngine;
 
-public class PlayerPieceAnimator : MonoBehaviour
+public class PlayerPieceAnimator : NetworkBehaviour
 {
     [SerializeField] private Animator animator;
+
+    [Networked, OnChangedRender(nameof(OnAnimationChanged))]
+    public Vector3 CurrentMovement { get; set; }
+    
     private readonly int HashX = Animator.StringToHash("X");
     private readonly int HashY = Animator.StringToHash("Y");
+    
 
     public void SetMovement(Vector2 movement)
     {
+        if (HasStateAuthority == false)
+        {
+            Debug.Log("no authority");
+            return;
+        }
+        
+        CurrentMovement = new Vector3(movement.x, 0, movement.y);
+        
         animator.SetFloat(HashX, movement.x);
         animator.SetFloat(HashY, movement.y);
+    }
+
+    private void OnAnimationChanged()
+    {        
+        if (HasStateAuthority)
+            return;
+        animator.SetFloat(HashX, CurrentMovement.x);
+        animator.SetFloat(HashY, CurrentMovement.y);
     }
 }
