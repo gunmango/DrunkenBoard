0c30506e59a0256c3bfbc06ecc6e30712a7e75a4
gunmango <iamdoorpark@gmail.com>
2025-06-09 09:29:58 +0900

    시연용 주사위 정하기

--- DrunkenBoard/Assets/Scripts/MainGameScene/Board/BoardGamePlayer.cs
+++ DrunkenBoard/Assets/Scripts/MainGameScene/Board/BoardGamePlayer.cs
@@ -11,6 +11,7 @@ public class BoardGamePlayer : ATurnPlayer
     private PlayerPiece _piece = null;
     private bool _clicked = false;
     private int _diceResult = 0;
+    private int _overrideDiceResult = -1;
 
     public void Initialize(DiceSetter diceSetter, DiceDisplayer diceDisplayer, TurnSystem turnSystem, int uuid)
     {
@@ -44,13 +45,14 @@ public class BoardGamePlayer : ATurnPlayer
         //주사위 굴리고
         yield return new WaitWhile(() => _diceDisplayer.IsRolling);
         rollDiceButton.gameObject.SetActive(true);
+        _overrideDiceResult = -1;
         WebCamStartBlinking_RPC();
         
         yield return new WaitUntil(() => _clicked);
         yield return new WaitWhile(() => _diceDisplayer.IsRolling);
         
         //말 움직이기
-        _diceResult = _diceSetter.DiceResult;
+        _diceResult = _overrideDiceResult == -1 ? _diceSetter.DiceResult : _overrideDiceResult;
         yield return StartCoroutine(_piece.MoveSpace(_diceResult));
         
         WebCamStopBlinking_RPC();
@@ -64,6 +66,17 @@ public class BoardGamePlayer : ATurnPlayer
         MainGameSceneManager.GameStateManager.ActOnBoard -= () => ready = true;
         _clicked = false;
         EndTurn();
+    }
+
+    private void Update()
+    {
+        for (int i = 1; i <= 6; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            {
+                _overrideDiceResult = i;
+            } 
+        }    
     }
 
 
